0ce395dc34c2746bb8b9707ef21cf024d2fccf37
EduCamelo <eduardocamelobs@gmail.com>
2024-11-22 15:06:15 -0300

    Basico do Models do CI(sem o CI ainda)

---
+++ CodeIgniter3/Medico.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Medico extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function criar($CRM, $espec) 
+    {
+      $data["CRM"] = $CRM
+      $data["espec"] = $espec
+        return $this->db->insert('medico', $data);
+    }
+
+    public function update_medico($id_medico, $CRM, $espec) {
+        $data["CRM"] = $CRM
+        $data["espec"] = $espec
+        $this->db->where('id_medico', $id_medico);
+        $this->db->update('medico', $data);
+    }
+
+    public function deletar($id_medico)
+    {
+        $this->db->where('id_medico', $id_medico);
+        return $this->db->delete('medico');
+    }
+
+
+    public function get_all_medicos()
+    {
+        $query = $this->db->get('medico');
+        return $query->result_array();
+    }
+}
---
+++ CodeIgniter3/item.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class item extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function criar($nome_item, $descricao, $preco, $quantidade) 
+    {
+      $data["nome_item"] = $nome_item
+      $data["descricao"] = $descricao
+      $data["preco"] = $preco
+      $data["quantidade"] = $quantidade 
+        return $this->db->insert('item', $data);
+    }
+
+    public function update_item($id_item, $descricao, $preco, $quantidade) {
+        $data["nome_item"] = $nome_item
+        $data["descricao"] = $descricao
+        $data["preco"] = $preco
+        $data["quantidade"] = $quantidade 
+        $this->db->where('id_item', $id_item);
+        $this->db->update('item', $data);
+    }
+
+    public function deletar($id_item)
+    {
+        $this->db->where('id_item', $id_item);
+        return $this->db->delete('item');
+    }
+
+
+    public function get_all_items()
+    {
+        $query = $this->db->get('item');
+        return $query->result_array();
+    }
+}
